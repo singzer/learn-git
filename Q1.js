e3eef6e0d892bc94256a6f4352edbdf0adf8c059
李白 <2895585936@qq.com>
2021-11-26 16:28:28 +0800

    feat:增加新功能

--- Q1.js
+++ Q1.js
@@ -1,7 +1,23 @@
-// 写一段你最拿手的 JavaScript 代码
+// 写一段你最拿手的 JavaScript 代码 
 
 // vhxubo
 console.log('Hello World!')
 
 // 昵称
+//lixao
 // 代码段, 无需注释
+(function () { 
+    var setFont = function () { 
+        var html = document.documentElement;  
+        var width = html.clientWidth;
+ 
+        if (width < 1024) width = 1024
+        if (width > 1920) width = 1920 
+        var fontSize = width / 80 + 'px'; 
+        html.style.fontSize = fontSize;
+    }
+    setFont(); 
+    window.onresize = function () {
+        setFont();
+    }
+})();
